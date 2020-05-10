e46371e703bc88bcaa35094d47e3db215b5d0603
791388069@qq.com <791388069@qq.com>
2020-05-10 16:29:16 +0800

    适配器模式         接口适配器模式

---
+++ src/com/water/adapter/interfaceadapter/AbsAdapter.java
@@ -0,0 +1,23 @@
+package com.water.adapter.interfaceadapter;
+
+public abstract class AbsAdapter implements Interfacetest {
+    @Override
+    public void method1() {
+
+    }
+
+    @Override
+    public void method2() {
+
+    }
+
+    @Override
+    public void method3() {
+
+    }
+
+    @Override
+    public void method4() {
+
+    }
+}
---
+++ src/com/water/adapter/interfaceadapter/Interfacetest.java
@@ -0,0 +1,11 @@
+package com.water.adapter.interfaceadapter;
+
+public interface Interfacetest {
+    void method1();
+
+    void method2();
+
+    void method3();
+
+    void method4();
+}
