ca405b2e33a725c80466f14d423e10d4aa69865c
791388069@qq.com <791388069@qq.com>
2020-05-11 09:54:30 +0800

    状态模式 每进一次循环，相当于一次活动的实例。上次实例的状态信息会传到下次的实例中来。

--- src/com/water/state/DispenseOutState.java
+++ src/com/water/state/DispenseOutState.java
@@ -1,5 +1,7 @@
 package com.water.state;
 
+import javax.swing.plaf.synth.SynthEditorPaneUI;
+
 public class DispenseOutState implements State {
     RaffleActivity activity;
 
@@ -10,19 +12,20 @@ public class DispenseOutState implements State {
     @Override
     public void deduceMoney() {
 
-        System.out.println("奖品已发完请下次参加");
+        System.out.println("扣积分环节-----------奖品已发完请下次参加");
     }
 
     @Override
     public boolean raffle() {
 
-        System.out.println("奖品已发完请下次参加");
+        System.out.println("抽奖环节-------奖品已发完请下次参加");
+        System.exit(0);
         return false;
     }
 
     @Override
     public void dispensePrize() {
 
-        System.out.println("奖品已发完请下次参加");
+        System.out.println("发放奖品环节-----------奖品已发完请下次参加");
     }
 }
